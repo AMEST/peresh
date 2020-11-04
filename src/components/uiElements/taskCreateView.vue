7236042a07533e8d369e238a2240f93852f7f32d
Klabukov Erik <AMECT@ya.ru>
2020-11-04 14:04:03 +0500

    Mark down editor and fixes (#9)
    * Replaced the usual input field for summary with a visual markdown editor
    
    * Фикс End date при редактировании
    
    * Delete old task summary text area

--- src/components/uiElements/taskCreateView.vue
+++ src/components/uiElements/taskCreateView.vue
@@ -24,7 +24,9 @@
         </div>
         <div class="row mb-3">
             <div class="col-2 min-width-col"> {{ $Lang.summary }}</div>
-            <div class="col-10"><textarea class="form-control" v-model="currentTask.summary" v-bind:placeholder="$Lang.summary+' Markdown works'" rows="7"></textarea></div>
+            <div class="col-10">
+                <vue-simplemde v-model="currentTask.summary" ref="markdownEditor" />
+            </div>
         </div>
         <div class="row mb-3">
             <div class="col-2 min-width-col"></div>
@@ -43,6 +45,7 @@
 import dateTimePicker from 'vue-bootstrap-datetimepicker';
 // Import date picker css
 import 'pc-bootstrap4-datetimepicker/build/css/bootstrap-datetimepicker.css';
+import VueSimplemde from 'vue-simplemde'
 export default {
     name:"taskCreateView",
     props: ["mode"],
@@ -56,7 +59,8 @@ export default {
       }
     },
     components:{
-        dateTimePicker
+        dateTimePicker,
+        VueSimplemde
     },
     methods:{
         createNewTask: function(){
@@ -88,6 +92,7 @@ export default {
             }
             return name;
         },
+        // eslint-disable-next-line
         endDateChanged: function(event){
             var self = this
             self.currentTask.expiry = new Date(self.date).getTime()
@@ -103,6 +108,8 @@ export default {
             self.currentTask.status = "do"
             delete self.currentTask.customStatus
             self.currentTask.created = new Date().getTime()
+        }else{
+            self.date = new Date(self.currentTask.expiry)
         }
     },
     watch: { 
@@ -122,10 +129,18 @@ export default {
 }
 </script>
 <style>
+@import '~simplemde/dist/simplemde.min.css';
 .min-width-col{
     min-width: 85px;
 }
 .pb-for-oneline{
     padding-bottom: 80px;
+}
+.editor-toolbar{
+    background-color: #f8f9fa;
+}
+.CodeMirror, .CodeMirror-scroll {
+  max-height: 330px !important;
+  min-height: 200px !important;
 }
 </style>
